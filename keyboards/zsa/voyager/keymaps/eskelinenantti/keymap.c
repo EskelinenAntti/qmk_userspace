3035418b087478ee7db1df2354b635d5b7f7a804
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-08-01 18:30:00 +0300

    show typing heatmap animation at start

--- keyboards/zsa/voyager/keymaps/eskelinenantti/keymap.c
+++ keyboards/zsa/voyager/keymaps/eskelinenantti/keymap.c
@@ -37,7 +37,8 @@ RGB hsv_to_rgb_with_value(HSV hsv) {
 }
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_enable();
+    rgb_matrix_enable_noeeprom();
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_TYPING_HEATMAP);
 }
 
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
